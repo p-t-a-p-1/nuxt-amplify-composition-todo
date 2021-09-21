b2ef0971ef09d19d8c13245d88cfb86671125e33
p-t-a-p-1 <kb240225+1@gmail.com>
2021-09-21 22:38:38 +0900

    first commit.

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12" xs="12" sm="12" md="4" lg="4" xl="4" >
+        <v-form></v-form>
+      </v-col>
+      <v-col cols="12" xs="12" sm="12" md="8" lg="8" xl="8" >
+        <v-list>
+          <v-list-item></v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {}
+});
+</script>
